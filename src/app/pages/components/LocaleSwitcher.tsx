4ff1b013aa142ecb547943a0732d8b8869f2c720
ensp1re <ostapuk1318@gmail.com>
2024-11-19 21:41:12 +0200

    removed unused library

--- src/app/pages/components/LocaleSwitcher.tsx
+++ src/app/pages/components/LocaleSwitcher.tsx
@@ -1,4 +1,4 @@
-import { useLocale, useTranslations } from "next-intl";
+import { useLocale } from "next-intl";
 import LocaleSwitcherSelect from "./LocaleSwitcherSelect";
 
 export default function LocaleSwitcher() {
